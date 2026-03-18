4478b2cfe485139e96e049dc125eed946cfb8277
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2026-03-18 22:17:02 +0400

    Added content

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,40 @@
 // import Image from "next/image";cls
 import { ArrowUpRight } from 'lucide-react'
-
+import Link from 'next/link';
 export default function Home() {
   return (
     <div className="max-w-2xl mx-auto px-6 py-24 md:py-40">
+
+      {/* header */}
       <header className="flex flex-col items-center text-center mb-32">
         <div className="w-20 h-20 rounded-full border border-black/10 flex items-center justify-center overflow-hidden mb-8 grayscale hover:grayscale-0 transition-all duration-500">
           <img alt="Lasha Ergeshidze" className="w-full h-full object-cover" src="https://media.licdn.com/dms/image/v2/D4D03AQEYKFC3UnyRuQ/profile-displayphoto-scale_200_200/B4DZns85VfGkAY-/0/1760617011160?e=1775692800&v=beta&t=TlmVRiieaJi1iqb0pel0CZiSDYcb3W1IGpCd_KiBCks" />
         </div>
         <div className="space-y-4">
           <h1 className="text-4xl md:text-5xl font-extrabold tracking-tighter uppercase italic">Lasha Ergeshiidze</h1>
-          <p className="text-xs font-bold uppercase tracking-[0.3em] text-stark-gray">Front end devloper • Systems Architect</p>
-          <div className="inline-flex items-center gap-2 border border-black/10 px-3 py-1.5">
+          <p className="text-xs font-bold uppercase tracking-[0.3em] text-stark-gray">Front end devloper</p>
+          {/* <div className="inline-flex items-center gap-2 border border-black/10 px-3 py-1.5">
             <span className="blinking-dot"></span>
             <span className="text-[10px] font-black uppercase tracking-widest">Available for hire</span>
-          </div>
+          </div> */}
         </div>
       </header>
+
+      {/* about */}
       <section className="mb-32">
-        <h2 className="text-[10px] font-black uppercase tracking-[0.4em] text-stark-gray mb-8">Statement</h2>
+        <h2 className="text-[10px] font-black uppercase tracking-[0.4em] text-stark-gray mb-8">About</h2>
         <p className="text-xl md:text-2xl font-light leading-tight tracking-tight">
-          Engineering high-performance distributed systems. Focused on <span className="bg-accent px-1 font-semibold">scalability</span>, <span className="bg-accent px-1 font-semibold">latency</span>, and minimalist technical excellence.
+          Junior Front-End Developer working with{" "}
+          <span className="bg-accent px-1 font-semibold">React</span> and{" "}
+          <span className="bg-accent px-1 font-semibold">Next.js</span>.
+          Learning backend with{" "}
+          <span className="bg-accent px-1 font-semibold">Python</span> and{" "}
+          <span className="bg-accent px-1 font-semibold">Django</span>.
+          Highly motivated, eager to learn, and passionate about building scalable web apps.
         </p>
       </section>
+      {/* skils */}
+
       <section className="mb-32">
         <h2 className="text-[10px] font-black uppercase tracking-[0.4em] text-stark-gray mb-10">Capabilities</h2>
         <div className="grid grid-cols-2 gap-x-12 gap-y-8">
@@ -31,47 +43,122 @@ export default function Home() {
             <ul className="text-sm space-y-2 font-medium">
               <li>React / Next.js</li>
               <li>TypeScript</li>
-              <li>Low-latency UI</li>
+              <li>Redux Toolkit</li>
+              <li>Tailwind CSS</li>
+              <li>SCSS</li>
             </ul>
           </div>
           <div>
             <h3 className="text-xs font-bold uppercase mb-4 border-l-2 border-accent pl-3">Back</h3>
             <ul className="text-sm space-y-2 font-medium">
-              <li>Distributed Node.js</li>
-              <li>PostgreSQL / Redis</li>
-              <li>Go Systems</li>
+              <li>Python</li>
+              <li>Django</li>
+              <li>DRF</li>
+              <li>SQLite</li>
+              <li>Neon</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-xs font-bold uppercase mb-4 border-l-2 border-accent pl-3">Hosting</h3>
+            <ul className="text-sm space-y-2 font-medium">
+              <li>AWS</li>
+              <li>Netlify</li>
+              <li>Vercel</li>
+              <li>Render</li>
             </ul>
           </div>
         </div>
       </section>
+
+      {/* projects */}
       <section className="mb-32">
         <h2 className="text-[10px] font-black uppercase tracking-[0.4em] text-stark-gray mb-6">Selected Works</h2>
         <div className="flex flex-col">
-          <div className="minimal-card group hover:ml-1 transition-all cursor-pointer">
-            <div className="flex justify-between items-start mb-4">
-              <div className="flex items-center gap-3">
-                <span className="blinking-dot"></span>
-                <h3 className="text-lg font-extrabold uppercase tracking-tighter group-hover:underline decoration-accent decoration-2 cursorpoin">Nexus ERP</h3>
+
+          <a href="https://itjobsgeorgia.netlify.app/">
+            <div className="minimal-card group hover:ml-1 transition-all cursor-pointer">
+              <div className="flex justify-between items-start mb-4">
+                <div className="flex items-center gap-3">
+                  <span className="blinking-dot"></span>
+                  <h3 className="text-lg font-extrabold uppercase tracking-tighter group-hover:underline decoration-accent decoration-2 cursorpoin">ITJobsGeorgia</h3>
+                </div>
+                <span className="material-symbols-outlined text-sm font-light group-hover:mr-1 transition-all">
+                  <ArrowUpRight width={14} height={20} />
+                </span>
               </div>
-              <span className="material-symbols-outlined text-sm font-light group-hover:mr-1 transition-all"><ArrowUpRight width={14} height={20} /></span>
+              <p className="text-sm text-stark-gray font-medium max-w-md">
+                A website that scrapes and displays updated job listings, integrating React frontend with Flask backend and automated Python scraping.
+              </p>
+              <div className="flex flex-wrap gap-2 mt-4">
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">React</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">Typescript</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">React Router</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">Tailwind CSS</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">React bits</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">Python</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">Flask</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">Scrapy</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">bs4</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">Github Actions</span>
+              </div>
             </div>
-            <p className="text-sm text-stark-gray font-medium max-w-md">Real-time enterprise orchestration. Automated financial intelligence.</p><div className="flex flex-wrap gap-2 mt-4">
-              <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">React</span>
-              <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">Node.js</span>
-              <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">PostgreSQL</span>
-            </div>
-          </div>
+          </a>
 
+          <a href="https://heliotravel.ge/">
+            <div className="minimal-card group hover:ml-1 transition-all cursor-pointer">
+              <div className="flex justify-between items-start mb-4">
+                <div className="flex items-center gap-3">
+                  <span className="blinking-dot"></span>
+                  <h3 className="text-lg font-extrabold uppercase tracking-tighter group-hover:underline decoration-accent decoration-2 cursorpoin">Helio Travel</h3>
+                </div>
+                <span className="material-symbols-outlined text-sm font-light group-hover:mr-1 transition-all"><ArrowUpRight width={14} height={20} /></span>
+              </div>
+              <p className="text-sm text-stark-gray font-medium max-w-md">
+
+                Helio Travel is a fast, modern tourist agency website built with JavaScript and SCSS, powered by Decap CMS
+                and
+                hosted on Netlify for easy content management and deployment.
+
+              </p><div className="flex flex-wrap gap-2 mt-4">
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">JavaScript</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">Decap CMS</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">SCSS</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">Netlify</span>
+
+              </div>
+            </div>
+          </a>
+
+          <a href="https://smarthomeclone.netlify.app/">
+            <div className="minimal-card group hover:ml-1 transition-all cursor-pointer">
+              <div className="flex justify-between items-start mb-4">
+                <div className="flex items-center gap-3">
+                  <span className="blinking-dot"></span>
+                  <h3 className="text-lg font-extrabold uppercase tracking-tighter group-hover:underline decoration-accent decoration-2 cursorpoin">
+                    Smart home
+                  </h3>
+                </div>
+                <span className="material-symbols-outlined text-sm font-light group-hover:mr-1 transition-all"><ArrowUpRight width={14} height={20} /></span>
+              </div>
+              <p className="text-sm text-stark-gray font-medium max-w-md">
+                Developed a fully responsive Smart Home website using JavaScript and SCSS, featuring modern design and dynamic content across devices.
+              </p><div className="flex flex-wrap gap-2 mt-4">
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">JavaScript</span>
+                <span className="text-[10px] font-black uppercase tracking-widest bg-black/5 px-2 py-1 border border-black/5">SCSS</span>
+              </div>
+            </div>
+          </a>
         </div>
-
       </section>
+
       <footer className="pt-24 border-t border-black/5 flex flex-col md:flex-row justify-between items-center gap-8">
         <div className="flex gap-8 text-[10px] font-black uppercase tracking-widest">
-          <a className="hover:text-accent transition-colors" href="#">GitHub</a>
-          <a className="hover:text-accent transition-colors" href="#">LinkedIn</a>
+          <a className="hover:text-accent transition-colors" href="https://github.com/DarkLordGeo">GitHub</a>
+          <a className="hover:text-accent transition-colors" href="https://www.linkedin.com/in/lasha-ergeshidze-6822ba2a5/">LinkedIn</a>
         </div>
-        <p className="text-[10px] font-bold text-stark-gray uppercase tracking-widest">© 2024 RIVERASM — CORE v2.4</p>
+        <p className="text-[10px] font-bold text-stark-gray uppercase tracking-widest">© 2026 - All rights reserved</p>
       </footer>
+
     </div>
   );
 }
